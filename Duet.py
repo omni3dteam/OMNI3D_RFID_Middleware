a8fe11872b1751684aac55fccc7c7a9ea1274303
hb <hb@omni3d.net>
2023-05-17 15:12:16 +0200

    Getting rid of object model handling

--- Duet.py
+++ Duet.py
@@ -1,31 +1,7 @@
 from dsf.connections import CommandConnection
 from dsf.connections import SubscribeConnection, SubscriptionMode
 
-import logging
-from systemd.journal import JournalHandler
-
 import NFC
-
-EXTRUDER_0 = 0
-EXTRUDER_1 = 1
-BOTH_EXTRUDERS = 2
-NUMBER_OF_EXTRUDERS = 2
-
-
-def return_from_object_model(thing):
-    subscribe_connection = SubscribeConnection(SubscriptionMode.PATCH)
-    subscribe_connection.connect()
-    try:
-        # Get the complete model once
-        object_model = subscribe_connection.get_object_model()
-
-        #print(object_model.move.extruders[0].position)
-
-        # Get multiple incremental updates, due to SubscriptionMode.PATCH, only a
-        # subset of the object model will be updated
-    finally:
-        subscribe_connection.close()
-        #print((object_model.job.file.file_name))
 
 def send_code_m5676(mcode):
     pass
@@ -36,7 +12,7 @@ def send_code_m5675(mcode):
 def send_code_m5674(mcode):
     pass
 
-#Send detected filament as gcode
+# Send detected filament as gcode
 def send_code_m5673(filament_data):
     command_connection = CommandConnection(debug=True)
     command_connection.connect()
@@ -52,7 +28,7 @@ def send_code_m5673(filament_data):
                                  filament_data.month,
                                  filament_data.year,
                                  filament_data.serial_number,))
-#Read filament as gcode
+# Read filament as gcode
 def mcode_5678(cde):
     new_filament = NFC.Filament([cde.parameter("S").as_int(), 
                                  cde.parameter("F").as_int(),
